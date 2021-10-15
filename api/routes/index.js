1935cbb335a87f7fe5030048e99662eb8d02bd42
Falarad <pimentel611@gmail.com>
2021-10-15 01:18:03 -0400

    Following a tutorial, gonna edit to needs later

---
+++ api/routes/index.js
@@ -0,0 +1,21 @@
+const ctrlAuth = require('../controllers/authentication');
+const ctrlProfile = require('../controllers/profile');
+
+const express = require('express');
+const router = express.Router();
+
+// api/routes/index.js
+const jwt = require('express-jwt');
+
+const auth = jwt({
+  secret: process.env.SECRET,
+  userProperty: 'payload'
+});
+// profile
+router.get('/profile', auth, ctrlProfile.profileRead);
+
+// authentication
+router.post('/register', ctrlAuth.register);
+router.post('/login', ctrlAuth.login);
+
+module.exports = router;
